0d6b8280a3876564532b533e458540b0af55ca8d
Wuchang325 <kxxpg666@qq.com>
2025-08-05 11:33:38 +0800

    添加些内容

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -19,14 +19,15 @@ import sidebar from './sidebar'
  * @see https://theme-plume.vuejs.press/config/basic/
  */
 export default defineThemeConfig({
-  logo: 'https://theme-plume.vuejs.press/plume.png',
+  logo: './favicon.ico',
 
   appearance: true,  // 配置 深色模式
 
   social: [
-    { icon: 'github', link: '/' },
+    { icon: 'github', link: 'https://github.com/Wuchang325/docs.zient.top' },
+    { icon: 'qq', link: 'https://qm.qq.com/q/VgCWplffmU' },
   ],
-   navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
+   navbarSocialInclude: ['github', 'qq'], // 允许显示在导航栏的 social 社交链接
    aside: true, // 页内侧边栏， 默认显示在右侧
    outline: [2, 3], // 页内大纲， 默认显示 h2, h3
 
@@ -50,7 +51,7 @@ export default defineThemeConfig({
    * @see https://theme-plume.vuejs.press/config/basic/#profile
    */
   profile: {
-    avatar: 'https://theme-plume.vuejs.press/plume.png',
+    avatar: './favicon.ico',
     name: '辰砂屿·文档站',
     description: 'Minecraft公益服务器',
     // circle: true,
--- docs/README.md
+++ docs/README.md
@@ -1,20 +1,32 @@
 ---
 home: true
 config:
-  -
-    type: doc-hero
+  - type: doc-hero
     hero:
       name: 辰砂屿·文档站
       text: Minecraft公益服务器
       tagline: 为游玩辰砂屿服务器提供文档辅助
-      image: /plume.png
+      image: /logo.png
       actions:
-        -
-          theme: brand
-          text: 快速开始 →
+        - theme: brand
+          text: 开始阅读
           link: /guide/
-        -
-          theme: alt
-          text: Github
+        - theme: brand
+          text: 加入交流群
+          link: https://qm.qq.com/q/VgCWplffmU
+        - theme: alt
+          text: GitHub
           link: https://github.com/Wuchang325/docs.zient.top
+
+  - type: features
+    features:
+      - title: 公益
+        icon: 🌳
+        details: 我们承诺，服务器始终不会出现商业行为
+      - title: 友好
+        icon: 👥
+        details: 努力建设和谐玩家社区
+      - title: 自由
+        icon: 🤝
+        details: 不会过度干涉，保证玩家自由
 ---
